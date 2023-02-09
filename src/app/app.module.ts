a7de3dd3d6b48459b2442c63394d149a9e183c9c
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-02-09 10:31:21 +0100

    home page init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { FaqComponent } from './components/home/faq/faq.component';
+import { CtaComponent } from './components/home/cta/cta.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CaraComponent } from './components/home/cara/cara.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    FaqComponent,
+    CtaComponent,
+    HomeComponent,
+    CaraComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    "./src/**/*.{html,ts}",
+],
+  theme: {
+    extend: {
+       colors: {
+      'heroColor': '#fcf8f5',
+      
+    },},
+  },
+  plugins: [],
+}
