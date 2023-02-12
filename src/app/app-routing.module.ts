1854fc1d04ad65d4c2485d10661004fd9d910662
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-02-12 13:51:35 +0100

    game model and migration done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import { GamesComponent } from './pages/games/games.component';
+import { MyReservationsComponent } from './pages/my-reservations/my-reservations.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'myreservations', component: MyReservationsComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { Cta2Component } from './components/home/cta2/cta2.component';
 import { TestimonialsComponent } from './components/home/testimonials/testimonials.component';
 import { PicLoginComponent } from './components/login/pic-login/pic-login.component';
+import { GamesComponent } from './pages/games/games.component';
+import { SearchbarComponent } from './components/Games/searchbar/searchbar.component';
+import { ListOfGamesComponent } from './components/Games/list-of-games/list-of-games.component';
+import { MyReservationsComponent } from './pages/my-reservations/my-reservations.component';
+import { ListOfReservationsComponent } from './components/myReservations/list-of-reservations/list-of-reservations.component';
+import { SearchBarComponent } from './components/myReservations/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +39,12 @@ import { PicLoginComponent } from './components/login/pic-login/pic-login.compon
     Cta2Component,
     TestimonialsComponent,
     PicLoginComponent,
+    GamesComponent,
+    SearchbarComponent,
+    ListOfGamesComponent,
+    MyReservationsComponent,
+    ListOfReservationsComponent,
+    SearchBarComponent,
    
   ],
   imports: [
