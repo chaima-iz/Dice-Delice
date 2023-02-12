f7db2522e11977c1082455faf43c6d18d540dfb7
chaima-iz <shaymaeizwaghne.01@gmail.com>
2023-02-12 14:15:22 +0100

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-<<<<<<< HEAD
 import { GamesComponent } from './pages/games/games.component';
 import { MyReservationsComponent } from './pages/my-reservations/my-reservations.component';
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'games', component: GamesComponent },
-  { path: 'myreservations', component: MyReservationsComponent },
-=======
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'myreservations', component: MyReservationsComponent },
   { path: 'signUp', component: SignUpComponent }
->>>>>>> 06a0c6460057f59055c9fc41f55601ab3c35c58a
 
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
