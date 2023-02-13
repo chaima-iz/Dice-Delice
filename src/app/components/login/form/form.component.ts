3ea647e1d9d78b742da68251653e587634a748ad
walid damou <walid.damou.2015@gmail.com>
2023-02-13 20:44:38 +0100

    Login API

--- src/app/components/login/form/form.component.ts
+++ src/app/components/login/form/form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginServiceService } from 'src/app/services/loginService/login-service.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
-
+export class FormComponent implements OnInit{
+  
+  email : string = '';
+  password : string = '';
+  constructor(private loginservice : LoginServiceService , private router:Router){ }
+  
+  ngOnInit(): void {
+    localStorage.setItem("logged_in" , "false");
+  }
+  submit(){
+    this.loginservice.login(this.email , this.password)
+    .subscribe(res=>{
+      if(res.user){
+        localStorage.setItem("logged_in" , "true");
+        this.router.navigate(['/'])
+      }
+    });
+  }
 }
