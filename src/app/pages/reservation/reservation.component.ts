5c65a4cfa6e0f9c289ac6c01f59a762380e144a0
walid damou <walid.damou.2015@gmail.com>
2023-02-20 21:25:59 +0100

    Add reservation backEnd

--- src/app/pages/reservation/reservation.component.ts
+++ src/app/pages/reservation/reservation.component.ts
@@ -1,35 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { GameService } from 'src/app/services/gameService/game.service';
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit{
   reservation_date:any;
   start_time:any;
   end_time:any;
-  game:any;
   payement:any;
   instructions:any;
-  
-  constructor(private dataService: DataService) {}
+  id_user:any;
+  id_game:any;
+
+  games : any;
+  user : any;
+
+  ngOnInit(): void {
+    this.gameService.listGames().subscribe(
+      res=>{
+        // console.log(res);
+        this.games = res;
+      }
+    );
+    this.user = localStorage.getItem('user');
+    this.user = JSON.parse(this.user);
+    this.id_user = this.user.id;
+    // console.log(this.user)
+  }
+  constructor(private dataService: DataService , private gameService : GameService) {}
   
   addReservation() {
     const reservationData = {
       reservation_date: this.reservation_date,
       start_time: this.start_time,
       end_time: this.end_time,
-      game: this.game,
+      id_game: this.id_game,
+      id_user: this.id_user,
       payement: this.payement,
       instructions: this.instructions
     };
-    
+    // console.log(reservationData);
     this.dataService.addReservation(reservationData)
       .subscribe(
         data => {
           window.location.reload();
-          console.log('it works');
         },
         error => {
           console.log('this doesnt work');
